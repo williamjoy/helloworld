2c9311054e3376dc4f18729220e98dfb531053a9
Lichun Wei <lichun.william@gmail.com>
2015-04-11 14:09:03 +0800

    aggregate by sender

--- parser.py
+++ parser.py
@@ -27,13 +27,29 @@ for subsystem in os.listdir(SRC_DIR):
 with open ('{}/{}'.format('by-signal', 'generated.yaml'),'wb') as yaml_file :
      yaml.dump(result,yaml_file,default_flow_style=False)
 
-csv_file = open('by-signal/generated.csv', 'wb')
-csv_file.write('Signal, Sub System, Function, Sender, Receiver\n')
+csv_file_all = open('by-signal/generated.csv', 'wb')
+csv_file_group_by_sender = open('by-signal/group_by_sender.csv', 'wb')
+csv_file_all.write('Signal, Sub System, Function, Sender, Receiver\n')
+
+group_by_sender= {}
 for signal in result:
+    if (signal not in group_by_sender):
+        group_by_sender[signal] = {}
+        group_by_sender[signal]['sender']   = set([])
+        group_by_sender[signal]['receiver'] = set([])
     for subsystem in result[signal]:
         for function in result[signal][subsystem]:
-            #print signal, subsystem, function , result[signal][subsystem][function] 
-            line = ','.join([signal, subsystem, function, '|'.join(result[signal][subsystem][function]['sender']), '|'.join(result[signal][subsystem][function]['receiver'])])
-            line += '\n'
-            csv_file.write(line)
+            sender   = result[signal][subsystem][function]['sender']
+            receiver = result[signal][subsystem][function]['receiver']
+            group_by_sender[signal]['sender']   |= set(sender)
+            group_by_sender[signal]['receiver'] |= set(receiver)
 
+            line = ','.join([signal, subsystem, function, '|'.join(sender), '|'.join(receiver)])
+            line += '\n'
+            csv_file_all.write(line)
+
+csv_file_group_by_sender.write('Signal, Sender, Receiver\n')
+for signal in group_by_sender:
+    csv_file_group_by_sender.write( '{},{},{}\n'.format( signal,
+                             '|'.join(list(group_by_sender[signal]['sender'])),
+                             '|'.join(list(group_by_sender[signal]['receiver'])) ))
