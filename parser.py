e6fa9876af93c18d4a275f328cc638cdc0ecc1b7
Lichun Wei <lichun.william@gmail.com>
2015-04-11 13:40:37 +0800

    csv file

--- parser.py
+++ parser.py
@@ -27,3 +27,13 @@ for subsystem in os.listdir(SRC_DIR):
 with open ('{}/{}'.format('by-signal', 'generated.yaml'),'wb') as yaml_file :
      yaml.dump(result,yaml_file,default_flow_style=False)
 
+csv_file = open('by-signal/generated.csv', 'wb')
+csv_file.write('Signal, Sub System, Function, Sender, Receiver\n')
+for signal in result:
+    for subsystem in result[signal]:
+        for function in result[signal][subsystem]:
+            #print signal, subsystem, function , result[signal][subsystem][function] 
+            line = ','.join([signal, subsystem, function, '|'.join(result[signal][subsystem][function]['sender']), '|'.join(result[signal][subsystem][function]['receiver'])])
+            line += '\n'
+            csv_file.write(line)
+
