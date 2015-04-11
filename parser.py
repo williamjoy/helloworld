1cb858bda754872e52e0d68304b12753b6d1d5b2
Lichun Wei <lichun.william@gmail.com>
2015-04-11 12:40:07 +0800

    parse all subsys

--- parser.py
+++ parser.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python
 
 import yaml
+import os
 
-data = yaml.load(open('subsystem1.yaml'))
+SRC_DIR = './src'
 
 result = {}
-for function in  data:
-    for signal in data[function]:
-        ECUs = data[function][signal]
-        if (signal not in result):
-            result[signal] = {}
-        if (function not in result[signal]):
-            result[signal][function] = {}
-            result[signal][function]['sender']   = []
-            result[signal][function]['receiver'] = []
-        result[signal][function]['sender'].append(ECUs[0])
-        for receiver in ECUs[1:]:
-            result[signal][function]['receiver'].append(receiver)
+for subsystem in os.listdir(SRC_DIR):
+    data = yaml.load(open('{}/{}'.format(SRC_DIR, subsystem)))
 
-with open ('{}/{}'.format('by-signal', 'subsystem1.yaml'),'wb') as yaml_file :
+    for function in  data:
+        for signal in data[function]:
+            ECUs = data[function][signal]
+            if (signal not in result):
+                result[signal] = {}
+            if (subsystem not in result[signal]):
+                result[signal][subsystem] = {}
+            if (function not in result[signal][subsystem]):
+                result[signal][subsystem][function] = {}
+                result[signal][subsystem][function]['sender']   = []
+                result[signal][subsystem][function]['receiver'] = []
+            result[signal][subsystem][function]['sender'].append(ECUs[0])
+            for receiver in ECUs[1:]:
+                result[signal][subsystem][function]['receiver'].append(receiver)
+
+with open ('{}/{}'.format('by-signal', 'generated.yaml'),'wb') as yaml_file :
      yaml.dump(result,yaml_file,default_flow_style=False)
 
