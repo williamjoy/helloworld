bcc193b6a8783501534fd56e3d17c670b8ad62a0
Lichun Wei <lichun.william@gmail.com>
2015-04-28 23:16:16 +0800

    print output to markdown to show how data is processed

--- import-csv.py
+++ import-csv.py
@@ -5,14 +5,24 @@ import yaml
 
 parsed_data = {}
 input_dir = './input'
+log_md=open("output.md", 'wb')
+
 for subsystem in os.listdir(input_dir):
     if(not subsystem.endswith('.csv')):
         continue
+    log_md.write("{}\n==========================\n".format(subsystem))
     with open("{}/{}".format(input_dir,subsystem), 'rb') as csvfile:
         subsystem = subsystem[:-4]
         reader = csv.DictReader(csvfile,skipinitialspace=True)
         print reader.fieldnames
+        log_md.write("|{}|\n| ".format("|".join(reader.fieldnames)))
+        for i in range(len(reader.fieldnames)):
+            log_md.write("--- |")
         for row in reader:
+            log_md.write("\n| ")
+            for i in range(len(reader.fieldnames)):
+                log_md.write(row[reader.fieldnames[i]])
+                log_md.write(" | ")
             signal_long_name = row['Long Name']
             parsed_data.setdefault(signal_long_name, {})
             parsed_data[signal_long_name].setdefault(subsystem, {})
@@ -20,6 +30,5 @@ for subsystem in os.listdir(input_dir):
             parsed_data[signal_long_name][subsystem]['Short Name'] = row['Short Name']
             parsed_data[signal_long_name][subsystem]['Subscriber'] += row['Subscriber'].split('|')
             parsed_data[signal_long_name][subsystem]['Subscriber'] = list(set(parsed_data[signal_long_name][subsystem]['Subscriber']))
-    print subsystem
 print '========================='
-print yaml.dump(parsed_data,default_flow_style=False)
+log_md.write('\n\nGenerated Data\n==========================\n\n```{}```'.format( yaml.dump(parsed_data,default_flow_style=False)))
---
+++ output.md
@@ -0,0 +1,34 @@
+project-ip360.csv
+==========================
+|Short Name|Long Name|Subscriber|Feature02:EBD|Feature05:ABS|Feature06:TCS|Feature07:SCS  |
+| --- |--- |--- |--- |--- |--- |--- |
+| AcceActPos | Accelerator Actual  Position | SCS | T | T | T | T | 
+| AcceActPosV | Accelerator Actual  Position Validity | SCS | T | T | T | T | 
+| AutocTrCmdGear | Automatic Transimission Commanded Gear | ECM | T | T | T |  | 
+| BrkPdlPos | Brake Pedal Position | GW|ECM |  |  |  | T | 
+
+Generated Data
+==========================
+
+```Accelerator Actual  Position:
+  project-ip360:
+    Short Name: AcceActPos
+    Subscriber:
+    - SCS
+Accelerator Actual  Position Validity:
+  project-ip360:
+    Short Name: AcceActPosV
+    Subscriber:
+    - SCS
+Automatic Transimission Commanded Gear:
+  project-ip360:
+    Short Name: AutocTrCmdGear
+    Subscriber:
+    - ECM
+Brake Pedal Position:
+  project-ip360:
+    Short Name: BrkPdlPos
+    Subscriber:
+    - GW
+    - ECM
+```
