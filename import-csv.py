50665ce77c9dc4cad2cbd9559f7ca39ac2634273
Lichun Wei <lichun.william@gmail.com>
2015-05-06 22:18:22 +0800

    set field name

--- import-csv.py
+++ import-csv.py
@@ -3,10 +3,16 @@ import csv
 import os
 import yaml
 
-parsed_data_by_sender = {}
+parsed_data_by_signal = {}
+parsed_data_by_publisher = {}
 parsed_data_by_subscriber = {}
 input_dir = './input'
 log_md=open("output.md", 'wb')
+
+__SHORT_NAME__ = 'Short Name'
+__LONG_NAME__  = 'Long Name'
+__SUBSCRIBER__ = 'Subscriber'
+__PUBLISHER__  = 'Publisher'
 
 for subsystem in os.listdir(input_dir):
     if(not subsystem.endswith('.csv')):
@@ -27,15 +33,17 @@ for subsystem in os.listdir(input_dir):
                     cell = '\\|'.join([ '`{}`'.format(i) for i in cell.split('|') ])
                 log_md.write(cell)
                 log_md.write(" |")
-            signal_long_name = row['Long Name']
-            parsed_data_by_sender.setdefault(signal_long_name, {})
-            parsed_data_by_sender[signal_long_name].setdefault(subsystem, {})
-            parsed_data_by_sender[signal_long_name][subsystem].setdefault('Subscriber', [])
-            parsed_data_by_sender[signal_long_name][subsystem]['Short Name'] = row['Short Name']
-            parsed_data_by_sender[signal_long_name][subsystem]['Publisher'] = row['Publisher']
-            subscribers = row['Subscriber'].split('|')
-            parsed_data_by_sender[signal_long_name][subsystem]['Subscriber'] += subscribers
-            parsed_data_by_sender[signal_long_name][subsystem]['Subscriber'] = list(set(parsed_data_by_sender[signal_long_name][subsystem]['Subscriber']))
+            signal_long_name = row[__LONG_NAME__]
+            parsed_data_by_signal.setdefault(signal_long_name, {})
+            parsed_data_by_publisher.setdefault(row[__SHORT_NAME__], {})
+            parsed_data_by_signal[signal_long_name].setdefault(subsystem, {})
+            parsed_data_by_signal[signal_long_name][subsystem].setdefault(__SUBSCRIBER__, [])
+            parsed_data_by_signal[signal_long_name][subsystem][__SHORT_NAME__] = row[__SHORT_NAME__]
+            parsed_data_by_signal[signal_long_name][subsystem][__PUBLISHER__] = row[__PUBLISHER__]
+            subscribers = row[__SUBSCRIBER__].split('|')
+            parsed_data_by_signal[signal_long_name][subsystem][__SUBSCRIBER__] += subscribers
+            parsed_data_by_signal[signal_long_name][subsystem][__SUBSCRIBER__] = list(set(parsed_data_by_signal[signal_long_name][subsystem]['Subscriber']))
     log_md.write("\n\n")
 print '========================='
-log_md.write('\nGenerated Data\n==========================\n\n```yaml\n{}```'.format( yaml.dump(parsed_data_by_sender,default_flow_style=False)))
+log_md.write('\nGenerated Data\n==========================\n\n```yaml\n')
+log_md.write('{}```'.format( yaml.dump(parsed_data_by_signal,default_flow_style=False)))
