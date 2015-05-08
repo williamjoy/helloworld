1c0c3aea7314ea91f9433154a400170f3e84e820
Lichun Wei <lichun.william@gmail.com>
2015-05-08 11:32:37 +0800

    generate graphviz dot source

--- import-csv.py
+++ import-csv.py
@@ -3,6 +3,11 @@ import csv
 import os
 import yaml
 
+from graphviz import Digraph
+import zlib
+
+dot = Digraph(comment='ECU Signals')
+
 by_signal = {}
 by_ecu = {}
 input_dir = './input'
@@ -14,6 +19,11 @@ __SUBSCRIBER__ = 'Subscriber'
 __PUBLISHER__  = 'Publisher'
 __SENDING__    = 'Sending'
 __RECEIVERING__  = 'Receiving'
+
+def hash_color(data):
+    return '#' + hex(zlib.adler32(data) % 0xffffff)[2:]
+
+all_ecus = set()
 
 for subsystem in os.listdir(input_dir):
     if(not subsystem.endswith('.csv')):
@@ -34,32 +44,46 @@ for subsystem in os.listdir(input_dir):
                     cell = '\\|'.join([ '`{}`'.format(i) for i in cell.split('|') ])
                 log_md.write(cell)
                 log_md.write(" |")
-            signal_long_name = row[__LONG_NAME__]
+
+            signal_long_name  = row[__LONG_NAME__]
+            signal_short_name = row[__SHORT_NAME__]
+            publisher   = row[__PUBLISHER__]
             subscribers = row[__SUBSCRIBER__].split('|')
+            if(publisher not in all_ecus):
+                all_ecus.add(publisher)
+                dot.node(publisher,publisher, style='filled', color=hash_color(publisher))
 
 
-            by_ecu.setdefault(row[__PUBLISHER__], {})
-            by_ecu[row[__PUBLISHER__]].setdefault(__SENDING__, {})
-            by_ecu[row[__PUBLISHER__]][__SENDING__].setdefault(signal_long_name, {})
-            by_ecu[row[__PUBLISHER__]][__SENDING__][signal_long_name].setdefault(subsystem, [])
-            by_ecu[row[__PUBLISHER__]][__SENDING__][signal_long_name][subsystem] += subscribers
-            by_ecu[row[__PUBLISHER__]][__SENDING__][signal_long_name][subsystem] = list(set(by_ecu[row[__PUBLISHER__]][__SENDING__][signal_long_name][subsystem]))
+            by_ecu.setdefault(publisher, {})
+            by_ecu[publisher].setdefault(__SENDING__, {})
+            by_ecu[publisher][__SENDING__].setdefault(signal_long_name, {})
+            by_ecu[publisher][__SENDING__][signal_long_name].setdefault(subsystem, [])
+            by_ecu[publisher][__SENDING__][signal_long_name][subsystem] += subscribers
+            by_ecu[publisher][__SENDING__][signal_long_name][subsystem] = list(set(by_ecu[publisher][__SENDING__][signal_long_name][subsystem]))
 
             for subscriber in subscribers:
+                if(subscriber not in all_ecus):
+                    all_ecus.add(subscriber)
+                    dot.node(subscriber,subscriber, style='filled', shape='box', color=hash_color(subscriber))
+                dot.edge(publisher,subscriber,label="{}::{}".format(subsystem,signal_short_name), color=hash_color(signal_short_name))
                 by_ecu.setdefault(subscriber, {})
                 by_ecu[subscriber].setdefault(__RECEIVERING__, {})
                 by_ecu[subscriber][__RECEIVERING__].setdefault(signal_long_name, {})
                 by_ecu[subscriber][__RECEIVERING__][signal_long_name].setdefault(subsystem,[])
-                by_ecu[subscriber][__RECEIVERING__][signal_long_name][subsystem].append(row[__PUBLISHER__]) #no dedup here, expecting array length to be 1
+                by_ecu[subscriber][__RECEIVERING__][signal_long_name][subsystem].append(publisher) #no dedup here, expecting array length to be 1
 
             by_signal.setdefault(signal_long_name, {})
             by_signal[signal_long_name].setdefault(subsystem, {})
             by_signal[signal_long_name][subsystem].setdefault(__SUBSCRIBER__, [])
-            by_signal[signal_long_name][subsystem][__SHORT_NAME__] = row[__SHORT_NAME__]
-            by_signal[signal_long_name][subsystem][__PUBLISHER__] = row[__PUBLISHER__]
+            by_signal[signal_long_name][subsystem][__SHORT_NAME__] = signal_short_name
+            by_signal[signal_long_name][subsystem][__PUBLISHER__] = publisher
             by_signal[signal_long_name][subsystem][__SUBSCRIBER__] += subscribers
             by_signal[signal_long_name][subsystem][__SUBSCRIBER__] = list(set(by_signal[signal_long_name][subsystem]['Subscriber']))
     log_md.write("\n\n")
 print '========================='
 log_md.write('\nGenerated Data\n==========================\n\n```yaml\n')
 log_md.write('{}```'.format( yaml.dump(by_ecu,default_flow_style=False)))
+
+log_md.write('\nGenerated Graphviz Source\n==========================\n\n```dot`\n')
+log_md.write(dot.source)
+log_md.write('\n```')
